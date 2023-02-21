948d038510849674187fa9078c22e327c81fa0ba
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-21 23:26:47 +0300

    Did task of sum numbers in an old array

---
+++ src/main/java/homework/day3/SumOfNumbers.java
@@ -0,0 +1,18 @@
+package homework.day3;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+        int[] number = new int[] {12,43,67,34};
+        int sum = 0;
+
+        for (int n : number) {
+
+            sum += n;
+            }
+
+            System.out.println("Сумма = " + sum);
+
+        }
+    }
+
