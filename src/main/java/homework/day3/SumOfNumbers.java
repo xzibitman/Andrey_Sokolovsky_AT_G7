ae4b22f8533115cff26d9d15cdb3d94f9bb10893
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-22 21:52:50 +0300

    add new logic in a method

--- src/main/java/homework/day3/SumOfNumbers.java
+++ src/main/java/homework/day3/SumOfNumbers.java
@@ -3,16 +3,16 @@ package homework.day3;
 public class SumOfNumbers {
     public static void main(String[] args) {
 
-        int[] number = new int[] {12,43,67,34};
+        int[] number = new int[]{12, 43, 67, 33, 99, 54};
         int sum = 0;
 
         for (int n : number) {
-
-            sum += n;
-            }
-
-            System.out.println("Сумма = " + sum);
-
+            if (n % 3 == 0)
+                sum += n;
         }
+
+        System.out.println("Сумма = " + sum);
+
     }
+}
 
