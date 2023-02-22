4e266cc3e066c4ca66f3c818c7bc06a76b33ad9a
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-22 23:09:23 +0300

    added new logic in array

--- src/main/java/homework/day3/SumOfNumbers.java
+++ src/main/java/homework/day3/SumOfNumbers.java
@@ -3,7 +3,7 @@ package homework.day3;
 public class SumOfNumbers {
     public static void main(String[] args) {
 
-        int[] number = new int[] {12,43,67,34};
+        int[] number = new int[] {12,43,67,34,126};
         int sum = 0;
 
         for (int n : number) {
