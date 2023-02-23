9c18c649118bf605012a7dbba9f2dbe247b7dbc0
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-23 21:11:51 +0300

    added new methods and variables in SparklingWater task

--- src/main/java/project/state/SparklingWater.java
+++ src/main/java/project/state/SparklingWater.java
@@ -1,16 +1,20 @@
 package project.state;
 
 public class SparklingWater extends Water {
-
+    private boolean isOpened = true;
     private Bubble[] bubbles;
     private String necessaryBubbles;
     private double bubbleAmount;
 
-    private double calcBubbleAmount(double volume) {
-        return volume * 10000;
-    }
+   // private double calcBubbleAmount(double volume) {
+   //     return volume * 10000;
+   // }
 
     public SparklingWater(double volume, String gas) {
+        private boolean isOpened();
+
+
+
         necessaryBubbles = gas;
         bubbleAmount = calcBubbleAmount(volume);
         bubbles = new Bubble[(int) bubbleAmount];
@@ -34,18 +38,23 @@ public class SparklingWater extends Water {
     public Bubble[] getBubbles() {
         return bubbles;
     }
+
     public void setBubbles(Bubble[] bubbles) {
         this.bubbles = bubbles;
     }
+
     public String getNecessaryBubbles() {
         return necessaryBubbles;
     }
-    public void setNecessaryBubbles(String necessaryBubbles ) {
+
+    public void setNecessaryBubbles(String necessaryBubbles) {
         this.necessaryBubbles = necessaryBubbles;
     }
+
     public double getBubbleAmount() {
         return bubbleAmount;
     }
+
     public void setBubbleAmount(double bubbleAmount) {
         this.bubbleAmount = bubbleAmount;
     }
