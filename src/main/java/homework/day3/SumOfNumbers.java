185d245b3dba00702408d720017fb26b5798a4d5
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-25 11:18:18 +0300

    added an if condition to search for a number

--- src/main/java/homework/day3/SumOfNumbers.java
+++ src/main/java/homework/day3/SumOfNumbers.java
@@ -3,16 +3,17 @@ package homework.day3;
 public class SumOfNumbers {
     public static void main(String[] args) {
 
-        int[] number = new int[] {12,43,67,34,126};
+        int[] number = new int[]{12, 43, 67, 34, 126};
         int sum = 0;
 
         for (int n : number) {
+            if (n % 3 == 0)
 
-            sum += n;
-            }
-
-            System.out.println("Сумма = " + sum);
-
+                sum += n;
         }
+
+        System.out.println("Сумма = " + sum);
+
     }
+}
 
