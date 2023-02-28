c40b7295055dfa99cad3dd524712093a6d087e3a
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-02-28 23:37:13 +0300

    did second task in playground

--- src/main/java/homework/day9/playground/essence/creatures/Vegetable.java
+++ src/main/java/homework/day9/playground/essence/creatures/Vegetable.java
@@ -1,4 +1,7 @@
 package homework.day9.playground.essence.creatures;
 
 public class Vegetable extends Plant{
+    public Vegetable(int mass, String name) {
+        super(mass, name);
+    }
 }
--- src/main/java/homework/day9/playground/essence/creatures/Vertebrata.java
+++ src/main/java/homework/day9/playground/essence/creatures/Vertebrata.java
@@ -1,4 +1,9 @@
 package homework.day9.playground.essence.creatures;
 
 public abstract class Vertebrata extends Animal {
+
+    public void eat(Insect food) {
+        String vertebrataText = String.format("I am %s and I am eating %s", getClass(),food);
+        System.out.println(vertebrataText);
+    }
 }
