02abc7cb8bde47758fc5556e6131c77b6b88b1ef
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-03-06 21:48:23 +0300

    fixed bugs

--- src/main/java/homework/day9/playground/essence/craft/air/Plane.java
+++ src/main/java/homework/day9/playground/essence/craft/air/Plane.java
@@ -1,7 +1,7 @@
 package homework.day9.playground.essence.craft.air;
 
-public class Airplane extends Aircraft {
-    public Airplane(int mass, String name) {
+public class Plane extends Aircraft {
+    public Plane(int mass, String name) {
         super(mass, name);
     }
 
