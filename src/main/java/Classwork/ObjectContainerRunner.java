6e81df82131d1687b80f2ffd4182c0ffd29a758a
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-03-11 15:09:59 +0300

    first class work

---
+++ src/main/java/Classwork/ObjectContainerRunner.java
@@ -0,0 +1,15 @@
+package Classwork;
+
+public class ObjectContainerRunner {
+    public static void main(String[] args) {
+        ObjectContainer objectContainer = new ObjectContainer();
+        for (int i = 0; i < 10; i++) {
+            objectContainer.add(i);
+        }
+
+        while (!objectContainer.isEmpty()) {
+            System.out.println(objectContainer.removeLast());
+        }
+
+    }
+}
