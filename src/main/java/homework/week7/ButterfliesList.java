690aad91a45483a08fdaf779357252afcf2a555e
Andrey_Sokolovsky <xzibitman023@gmail.com>
2023-03-20 23:10:35 +0300

    did task # 0-0.1-1-2 with arrays

---
+++ src/main/java/homework/week7/ButterfliesList.java
@@ -0,0 +1,30 @@
+package homework.week7;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ButterfliesList {
+    public static void main(String[] args) {
+        List<String> butterflies = new LinkedList<>();
+        butterflies.add("Common blue");
+        butterflies.add("Swallowtail");
+        butterflies.add("Aglais io");
+        butterflies.add("Common blue");
+        for (String text : butterflies) {
+            System.out.println("\"" + text + "\"");
+        }
+        int counter = 0;
+        for (String text : butterflies) {
+            if (text.contains("o")) {
+                counter++;
+            }
+        }
+        System.out.println("Букву О содержит - " + counter + " бабочки");
+        for (String text : butterflies) {
+            System.out.print(text + " ");
+        }
+        for (String text : butterflies) {
+            System.out.println("\n" + text);
+        }
+    }
+}
---
+++ src/main/java/homework/week7/FiguresList.java
@@ -0,0 +1,32 @@
+package homework.week7;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FiguresList {
+    public static void main(String[] args) {
+        List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
+        for (String text : figures) {
+            System.out.print(text + " - "); // удалить последний символ
+        }
+        int counter = 0;
+
+        for (String text : figures) {
+            if (text.contains("и")) {       // разобраться как написать НЕ содержит
+                counter++;
+            }
+        }
+        System.out.println(counter);
+        try {
+            figures.add(3, "Треугольник");
+            for (String text : figures) {
+                System.out.println(text + " ");
+            }
+
+        }
+        catch (UnsupportedOperationException e) {
+            System.out.println("нельзя добавить элемент в Arrays.asList");
+        }
+
+    }
+}
